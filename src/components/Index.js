125dec25f12cef873bc04f2368ef0ed751d99f49
Kiran Jadhav <jadhavrkiran21@gmail.com>
2024-03-03 12:25:53 +0530

    First commit

---
+++ src/components/Index.js
@@ -0,0 +1,8 @@
+import About from "./About/About";
+import Home from "./Home/Home";
+import Contact from "./Contact/Contact"
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import User from "./User/User";
+
+export{About,Home,Contact,Header,Footer, User}
